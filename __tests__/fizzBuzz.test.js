977d32cecb69a49d8b8d9d59312207acdfb595f1
Becker, Freya <freya.becker@lexisnexis.co.uk>
2020-08-26 19:14:31 +0100

    solution: test 2 - failing on Fizz

--- __tests__/fizzBuzz.test.js
+++ __tests__/fizzBuzz.test.js
@@ -5,3 +5,9 @@ test.each([[1, "1"], [2, "2"], [4, "4"], [6, "6"], [8, "8"]])(
     
     expect(fizzBuzzConvertNumber(numberToConvert)).toEqual(expectedValue);
 });
+
+test.each([3, 6, 9, 12])(
+    'given %p (a number divisible by 3) as argument, returns "Fizz"', (numberToConvert) => {
+    
+    expect(fizzBuzzConvertNumber(numberToConvert)).toEqual("Fizz");
+});
