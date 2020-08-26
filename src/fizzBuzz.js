d0a07e07ade2a551386a82977c30e16b0e138bab
Becker, Freya <freya.becker@lexisnexis.co.uk>
2020-08-26 19:30:05 +0100

    solution: test 4 - passing Fizzbuzz

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,4 +1,7 @@
 export function fizzBuzzConvertNumber(numberToConvert) {
+    if (numberToConvert % 3 === 0 && numberToConvert % 5 === 0) {
+        return "FizzBuzz";
+    }
     if (numberToConvert % 3 === 0) {
         return "Fizz";
     }
